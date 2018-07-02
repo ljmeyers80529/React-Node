cad865cfb295dd777a58509f16af3da8c77c929b
ljmeyers80529@mail.fresnosate.edu <ljmeyers80529@mail.fresnostate.edu>
2018-07-02 16:33:47 -0700

    Correction.

--- server-02/index.js
+++ server-02/index.js
@@ -6,6 +6,3 @@ const app = express();
 passport.use(new GoogleStrategy());
 
 app.listen(5000);
-
-// clientID: 763062955417-lg582de7tjudotpgq90scs3ns5r0uech.apps.googleusercontent.com
-// clientSecret: Fl0TFeirP04z0uHEFEMVk4hv
