53f56f4a7e154bbbf8f194500853284bea1ad790
ljmeyers80529@mail.fresnosate.edu <ljmeyers80529@mail.fresnostate.edu>
2018-07-02 19:51:40 -0700

    Saing model instance.

---
+++ server-09/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+require('./models/user');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
+
+const app = express();
+require('./routes/authRoutes')(app);
+app.listen(5000);
