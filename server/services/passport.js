8e5f004e37e42d5610810ff87fa26ecdf3469ce3
ljmeyers80529@mail.fresnosate.edu <ljmeyers80529@mail.fresnostate.edu>
2018-07-03 16:07:27 -0700

    Refactored to use async / await.

--- server/services/passport.js
+++ server/services/passport.js
@@ -15,20 +15,38 @@ passport.deserializeUser((id, done) => {
     });
 });
 
-passport.use(new GoogleStrategy({
+// passport.use(
+//     new GoogleStrategy({
+//     clientID: keys.googleClientID,
+//     clientSecret: keys.googleClientSecret,
+//     callbackURL: '/auth/google/callback'
+//     },
+//     (accessToken, refreshToken, profile, done) => {
+//         User.findOne({googleId: profile.id}).then((existingUser) => {
+//             if (existingUser) {
+//                 done(null, existingUser);
+//             } else {
+//                 new User({googleId: profile.id}).save()
+//                 .then(user => done(null, user));
+//             }
+//         });
+//     })
+// );
+
+
+passport.use(
+    new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
     },
-    (accessToken, refreshToken, profile, done) => {
-        User.findOne({googleId: profile.id}).then((existingUser) => {
-            if (existingUser) {
-                done(null, existingUser);
-            } else {
-                new User({googleId: profile.id}).save()
-                .then(user => done(null, user));
-            }
-        });
+    async (accessToken, refreshToken, profile, done) => {
+        const existingUser = await User.findOne({googleId: profile.id})
+        if (existingUser) {
+            return done(null, existingUser);
+        }
+        const user = await new User({googleId: profile.id}).save();
+        done(null, user);
     })
 );
 
