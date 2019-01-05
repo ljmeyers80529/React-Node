8147ed90013594a0cc4c6c3acb01a6e3d4434e8b
ljmeyers80529@mail.fresnosate.edu <ljmeyers80529@mail.fresnostate.edu>
2019-01-05 10:13:20 -0800

    updated to new versions

--- server/services/passport.js
+++ server/services/passport.js
@@ -15,25 +15,6 @@ passport.deserializeUser((id, done) => {
     });
 });
 
-// passport.use(
-//     new GoogleStrategy({
-//     clientID: keys.googleClientID,
-//     clientSecret: keys.googleClientSecret,
-//     callbackURL: '/auth/google/callback'
-//     },
-//     (accessToken, refreshToken, profile, done) => {
-//         User.findOne({googleId: profile.id}).then((existingUser) => {
-//             if (existingUser) {
-//                 done(null, existingUser);
-//             } else {
-//                 new User({googleId: profile.id}).save()
-//                 .then(user => done(null, user));
-//             }
-//         });
-//     })
-// );
-
-
 passport.use(
     new GoogleStrategy({
     clientID: keys.googleClientID,
